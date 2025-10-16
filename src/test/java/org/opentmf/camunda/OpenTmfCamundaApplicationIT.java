59c62d441da90c4464dc67e8be196ed125207b32
gokhanus <gokhan.demir@pia-team.com>
2025-10-17 00:43:18 +0300

    Upgrade to Camunda 7.24 Community

--- src/test/java/org/opentmf/camunda/OpenTmfCamundaApplicationIT.java
+++ src/test/java/org/opentmf/camunda/OpenTmfCamundaApplicationIT.java
@@ -1,15 +1,43 @@
 package org.opentmf.camunda;
 
+import dasniko.testcontainers.keycloak.KeycloakContainer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 @ActiveProfiles("it")
 class OpenTmfCamundaApplicationIT {
+
+  @Container
+  static final KeycloakContainer KEYCLOAK =
+      new KeycloakContainer("keycloak/keycloak:26.4.1")
+          .withAdminUsername("admin")
+          .withAdminPassword("admin")
+          .withRealmImportFile("realm/dsync-realm.json")
+          .waitingFor(Wait.forHttp("/realms/dsync/.well-known/openid-configuration"));
+
+  @DynamicPropertySource
+  static void registerProps(DynamicPropertyRegistry r) {
+    var issuer = String.format("http://%s:%d/realms/dsync",
+        KEYCLOAK.getHost(), KEYCLOAK.getMappedPort(8080));
+    var admin = String.format("http://%s:%d/admin/realms/dsync",
+        KEYCLOAK.getHost(), KEYCLOAK.getMappedPort(8080));
+
+    // Resource Server (JWT) and/or client config – adapt to your app:
+    r.add("plugin.identity.keycloak.keycloak-issuer-url", () -> issuer);
+    r.add("plugin.identity.keycloak.keycloak-admin-url", () -> admin);
+    // r.add("opentmf.security.jwk-set-uri", () -> issuer + "/protocol/openid-connect/certs");
+  }
 
   @Autowired
   private ApplicationContext applicationContext;
